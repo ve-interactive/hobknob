807c1e6a5eb9dc7a2f8527b454af4a6b9e2707eb
dsalanta <dsalanta@veinteractive.com>
2016-05-24 18:54:29 +0300

    client config based authentication provider logic

--- client/js/controllers/loginController.js
+++ client/js/controllers/loginController.js
@@ -1,12 +1,11 @@
 'use strict';
 
-featureToggleFrontend.controller('LoginController', ['$scope', function ($scope, $location) {
-    $scope.authProvider = getUrlParameter('authProvider');
-    
-    function getUrlParameter(name){
-        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        if (!results) { return 0; }
-        return decodeURIComponent(results[1].replace(/\+/g, " ")) || 0;
+featureToggleFrontend.controller('LoginController', ['$scope','$window','ENV', function ($scope, $window, ENV) {
+    if(ENV.RequiresAuth) {
+        $scope.authProvider = ENV.AuthProvider;
+    }
+    else {
+        $window.location.href = '/';
     }
     
 }]);
